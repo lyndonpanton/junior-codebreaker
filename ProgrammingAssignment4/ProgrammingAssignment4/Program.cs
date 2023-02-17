2bdaf6426b045e11c8231e4da1b47729312319d5
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-17 03:04:46 +0000

    Defined a data structure for the problem

--- ProgrammingAssignment4/ProgrammingAssignment4/Program.cs
+++ ProgrammingAssignment4/ProgrammingAssignment4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace ProgrammingAssignment4
 {
@@ -26,6 +28,36 @@ namespace ProgrammingAssignment4
 				// and the comment below. You can of
 				// course add more space between the
 				// comments as needed
+
+				Dictionary<char, int> characterCounter = new Dictionary<char, int>()
+				{
+					{ 'A', 0 },
+					{ 'B', 0 },
+					{ 'C', 0 },
+					{ 'D', 0 },
+					{ 'E', 0 },
+					{ 'F', 0 },
+					{ 'G', 0 },
+					{ 'H', 0 },
+					{ 'I', 0 },
+					{ 'J', 0 },
+					{ 'K', 0 },
+					{ 'L', 0 },
+					{ 'M', 0 },
+					{ 'N', 0 },
+					{ 'O', 0 },
+					{ 'P', 0 },
+					{ 'Q', 0 },
+					{ 'R', 0 },
+					{ 'S', 0 },
+					{ 'T', 0 },
+					{ 'U', 0 },
+					{ 'V', 0 },
+					{ 'W', 0 },
+					{ 'X', 0 },
+					{ 'Y', 0 },
+					{ 'Z', 0 }
+				};
 
 
 
